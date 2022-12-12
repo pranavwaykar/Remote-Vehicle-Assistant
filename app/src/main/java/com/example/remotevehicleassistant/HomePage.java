f4f1f388f85633f33a256d46c9035e610438707d
Pranav Waykar <waykarpranav777@gmail.com>
2022-12-12 17:26:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/remotevehicleassistant/HomePage.java
@@ -0,0 +1,44 @@
+package com.example.remotevehicleassistant;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class HomePage extends AppCompatActivity {
+ImageView mechanic,driver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_home_page);
+
+        mechanic = findViewById(R.id.mech);
+        driver = findViewById(R.id.driver);
+
+        mechanic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this, MechanicLogin.class);
+                startActivity(i);
+
+            }
+        });
+
+        driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this, Login.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+
+}
